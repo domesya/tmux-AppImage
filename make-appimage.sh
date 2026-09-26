0307b24b4f482adb0dc7482d44b550c5b7e04fe2
domesya <163451587+domesya@users.noreply.github.com>
2026-09-26 09:23:23 +0200

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -15,7 +15,8 @@ export MAIN_BIN=tmux
 # Deploy dependencies
 quick-sharun \
   /usr/bin/tmux \
-  /usr/lib/libncursesw.so* 
+  /usr/lib/libncursesw.so* \
+  /usr/lib/libsystemd.so*
 
 # Additional changes can be done in between here
 
@@ -24,4 +25,4 @@ quick-sharun --make-appimage
 
 # Test the app for 12 seconds, if the test fails due to the app
 # having issues running in the CI use --simple-test instead
-quick-sharun --test ./dist/*.AppImage
+quick-sharun --simple-test ./dist/*.AppImage
