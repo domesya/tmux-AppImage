c20b3d98e5917bf9cded9a05dabdcb5097885838
domesya <163451587+domesya@users.noreply.github.com>
2026-09-26 10:22:41 +0200

    Modify make-appimage.sh to disable VERSION export
    Comment out version retrieval and export for VERSION.

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,8 +3,9 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q tmux | awk '{print $2; exit}') # example command to get version of application here
-export ARCH VERSION
+# VERSION=$(pacman -Q tmux | awk '{print $2; exit}') # example command to get version of application here
+export ARCH
+# export VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
